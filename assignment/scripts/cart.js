672ae77e20b042ec65936528cf825f225f6c3c25
Brian Werner <brianmw199@gmail.com>
2023-10-11 11:30:29 -0500

    Created a global basket that is an empty array and added items to our basket with the addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,23 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// Create a global variable basket that is equal to []
+let basket = [];
+
+// Create a function named addItem that takes an input parameter for a string item
+// This function also will add a new item to the basket array and return true if item was added
+function addItem(item) {
+    basket.push(item);
+    return true;
+
+};
+console.log(`Basket is: ${basket}`);
+console.log('Adding Blackberries:', addItem('Blackberries'));
+console.log('Adding Delice de Bourgogne:', addItem('Delice de Bourgogne'));
+console.log('Adding Salami:', addItem('Salami'));
+console.log('Adding Crackers:', addItem('Crackers'));
+console.log('Adding Wine:', addItem('Wine'));
+console.log(`Basket is now ${basket}`);
 
 
 
