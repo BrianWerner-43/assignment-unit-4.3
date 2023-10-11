feea1ad2ccfe59a1a3e7c1dbc9607d328e6e7960
Brian Werner <brianmw199@gmail.com>
2023-10-11 14:38:52 -0500

    Created the empty and the isFull functions also updated the addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,9 +5,15 @@ console.log('***** Cart Functions *****');
 // Create a global variable basket that is equal to []
 let basket = [];
 
+// Create a global const named maxItems with the value of 5
+const maxItems = 5;
+
 // Create a function named addItem that takes an input parameter for a string item
 // This function also will add a new item to the basket array and return true if item was added
 function addItem(item) {
+    if (isFull()) {
+        return false;
+    }
     basket.push(item);
     return true;
 
@@ -18,6 +24,8 @@ console.log('Adding Delice de Bourgogne:', addItem('Delice de Bourgogne'));
 console.log('Adding Salami:', addItem('Salami'));
 console.log('Adding Crackers:', addItem('Crackers'));
 console.log('Adding Wine:', addItem('Wine'));
+// Testing the isFull function
+console.log('Add Ribeye: (should expect false):', addItem('Ribeye'));
 console.log(`Basket is now ${basket}`);
 
 // Create a function named listItems that will loop over the items in our basket.
@@ -28,15 +36,31 @@ function listItems() {
 
 };
 listItems();
-console.log('Inside the listitems function:', basket.length);
+console.log('Inside the listItems function:', basket.length);
 
 // Create a function named empty that should rest the basket to an empty array.
+function empty() {
+    basket = [];
 
+};
+empty();
+console.log('Inside the empty function:', basket);
 
+console.log('------Stretch Goals------');
 
+// Create a function named isFull that should return false if the basket is less than maxItems.
+// and return true otherwise if greater or equal to maxItems.
 
+function isFull() {
+    if (basket.length < maxItems) {
+        return false;
+    } if (basket.length >= maxItems) {
+        return true;
+    }
 
+};
 
+console.log('Inside the isFull function:', isFull());
 
 
 
